5f64b120d9bd7e70f03fc038207d3301bc2fd130
Davey-H-7 <138510681+Davey-H-7@users.noreply.github.com>
2024-01-10 12:22:02 +0000

    added type data

--- app/profiles/components/PokemonDetails.tsx
+++ app/profiles/components/PokemonDetails.tsx
@@ -4,13 +4,21 @@ export default async function PokemonDetails({name}:{name:string}) {
 
     const profileData = await getProfileData(name)
 
-    console.log(profileData);
+    console.log(profileData.types);
     const imgURL = profileData.sprites.other['official-artwork'].front_default
+    const primaryType = profileData.types[0].type.name
+    const secondaryType = profileData.types[1].type.name
 
     
     return (  
-        <div className="flex justify-center">
-        <Image src = {imgURL} width={500} height = {500} sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw" alt="an image of the pokemon"/>
+        <div className="Details flex flex-col justify-center">
+            <div className ="Types flex justify-center gap-5">
+                <p>Primary type: {primaryType} </p>
+                <p>Secondary type: {secondaryType} </p>
+            </div>
+            <div className="Image flex justify-center">
+                <Image src = {imgURL} width={500} height = {500} sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw" alt="an image of the pokemon"/>
+            </div>
         </div>
     );
 }
