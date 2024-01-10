6051a0e595046a831fd91acfabfeddaad9aadd09
Davey-H-7 <138510681+Davey-H-7@users.noreply.github.com>
2024-01-10 12:52:02 +0000

    added stats data for hp,attack, etc

--- app/profiles/components/PokemonDetails.tsx
+++ app/profiles/components/PokemonDetails.tsx
@@ -4,13 +4,22 @@ export default async function PokemonDetails({name}:{name:string}) {
 
     const profileData = await getProfileData(name)
 
-    console.log(profileData.types);
+    console.log(profileData.stats);
     const imgURL = profileData.sprites.other['official-artwork'].front_default
+
+
     const primaryType = profileData.types[0].type.name
     let secondaryType = null
 
     if (profileData.types[1]){
     secondaryType = profileData.types[1].type.name}
+
+    const hp = profileData.stats[0].base_stat
+    const attack = profileData.stats[1].base_stat
+    const defence = profileData.stats[2].base_stat
+    const spAttack = profileData.stats[3].base_stat
+    const spDefence = profileData.stats[4].base_stat
+    const speed = profileData.stats[5].base_stat
   
 
     
@@ -22,6 +31,14 @@ export default async function PokemonDetails({name}:{name:string}) {
             </div>
             <div className="Image flex justify-center">
                 <Image src = {imgURL} width={500} height = {500} sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw" alt="an image of the pokemon"/>
+            </div>
+            <div className ="Stats flex flex-col justify-center gap-5">
+                <p>HP: {hp} </p>
+                <p>Attack: {attack} </p>
+                <p>Defence: {defence} </p>
+                <p>Special-Attack: {spAttack} </p>
+                <p>Special-Defence: {spDefence} </p>
+                <p>Speed: {speed} </p>
             </div>
         </div>
     );
