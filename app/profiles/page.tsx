bfe5a375b20936f33f5ac764df56cbab4f32ed5e
Davey-H-7 <138510681+Davey-H-7@users.noreply.github.com>
2024-01-17 18:36:14 +0000

    name and types capitalized

--- app/profiles/page.tsx
+++ app/profiles/page.tsx
@@ -8,13 +8,14 @@ export default function Profile () {
 
   const searchParams = useSearchParams();
   
-  const pokemonName = searchParams.get("name") as string;
-
+  let pokemonName = searchParams.get("name") as string;
+  const pokemonNameCapitalized = pokemonName.charAt(0).toUpperCase() + pokemonName.slice(1);
+  
     return (
 
         <div className ="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8 text-center">
-        <h1 className="text-2xl font-light text-black sm:text-4xl sm:leading-tight mt-5"> {pokemonName} </h1>
-        <PokemonDetails name = {pokemonName}/>
+        <h1 className="text-2xl font-light text-black sm:text-4xl sm:leading-tight mt-5"> {pokemonNameCapitalized} </h1>
+        <PokemonDetails name = {pokemonName} />
         </div>
     )
 
