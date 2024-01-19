62112e6523f5468d2d440f7f4ae1e2d9f37074a1
Davey-H-7 <138510681+Davey-H-7@users.noreply.github.com>
2024-01-19 16:50:06 +0000

    stats seperated into PokemonStats component

--- app/profiles/components/PokemonDetails.tsx
+++ app/profiles/components/PokemonDetails.tsx
@@ -1,16 +1,12 @@
 import PokemonImage from "./PokemonImage";
 import PokemonTypes from "./PokemonTypes";
+import PokemonStats from "./PokemonStats";
 
 export default async function PokemonDetails({name}:{name:string}) {
 
     const profileData = await getProfileData(name)
 
-    const hp = profileData.stats[0].base_stat
-    const attack = profileData.stats[1].base_stat
-    const defence = profileData.stats[2].base_stat
-    const spAttack = profileData.stats[3].base_stat
-    const spDefence = profileData.stats[4].base_stat
-    const speed = profileData.stats[5].base_stat
+
   
 
     
@@ -18,15 +14,8 @@ export default async function PokemonDetails({name}:{name:string}) {
         <div className="Details flex flex-col justify-center">
             <PokemonTypes types ={profileData.types}/>
             <PokemonImage imgURL = {profileData.sprites.other['official-artwork'].front_default}/>
+            <PokemonStats stats = {profileData.stats}/>
 
-            <div className ="Stats flex flex-col justify-center gap-5">
-                <p>HP: {hp} </p>
-                <p>Attack: {attack} </p>
-                <p>Defence: {defence} </p>
-                <p>Special-Attack: {spAttack} </p>
-                <p>Special-Defence: {spDefence} </p>
-                <p>Speed: {speed} </p>
-            </div>
         </div>
     );
 }
